f8ccd839d6ad7b805beeedf1142696ebdf149bf2
Radek Izdebski <radek1izdebski@gmail.com>
2018-03-19 15:06:55 +0100

    Add more counters in script.js

--- script.js
+++ script.js
@@ -24,19 +24,51 @@ var Counter = React.createClass({
 	},
 	render: function() {
 		return React.createElement('div', {className: 'Counter'},
-			React.createElement('span', {}, 'Current result: ' + this.state.counter),
+			React.createElement('span', {}, 'Current result +/- 1: ' + this.state.counter),
 				React.createElement('div', {}, 'Counter +1',
 					React.createElement('button', {onClick: this.increment}, '+1')
 				),
 				React.createElement('div', {}, 'Counter -1',
 					React.createElement('button', {onClick: this.decrement}, '-1')
 				),
-				React.createElement('div', {}, 'Counter +6',
-					React.createElement('button', {onClick: this.incrementSix}, '+6')
-				)
-			
 		)
+	},
+
+	getDefaultProps: function() {
+		console.log('Tworzenie nowego komponentu: ustawia domyślne wartosci propsów, które nie zostały przekazane do komponentu');
+	},
+	componentWillMount: function() {
+		console.log('Tworzenie nowego komponentu: coś się dzieje - program pracuje i zaraz bedzie wynik - mozna to zakomuniować użytkownikowi');
+	},
+	componentDidMount: function() {
+		console.log('Tworzenie nowego komponentu: juz sie podziało, co się miało podziać, np. załadowal sie plik - mozna to zakomunikowac uzytkownikowi');
+	},
+	componentWillReceiveProps: function() {
+		console.log('Aktualizacja komponentu: zostanie wywołana tylko wtedy, gdy komponent otrzyma nowe właściwości; pozwala aktualizować stan na podstawie nadchodzących właściwości.');
+	},
+	shouldComponentUpdate: function() {
+		return(true);
+		console.log('Aktualizacja komponentu: sprawdz, czy trzeba ponownie przerenderowac komponent (if true), jezeli false, to blokuje działanie komponentu');
+	},
+	componentWillUpdate: function(){
+		console.log("Aktualizacja komponentu: wywoływana, kiedy sCU zwróci true");
+	},
+	componentDidUpdate: function () {
+		console.log('Aktualizacja komponentu: można manipulowac na DOM, np wyświetlić komunikat');
+	},
+	componentWillUnmount: function () {
+		console.log('Faza usuwania: ');
 	}
 });
-var element = React.createElement(Counter);
+
+
+
+var element = 
+React.createElement('div', {}, 
+	React.createElement(Counter),
+	React.createElement(Counter),
+	React.createElement(Counter)
+);
+
+
 ReactDOM.render(element, document.getElementById('app'));
